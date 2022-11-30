a3ce9407eca05be2b14b6688169e3251ece5df9c
Your Name <infor.emdiya@gmail.com>
2022-11-30 16:03:56 +0700

    Update

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -38,11 +38,14 @@ class _HomePageState extends State<HomePage> {
             );
           }),
           actions: [
-            IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset(
-                'assets/svg/notification_icon.svg',
-                height: 24,
+            Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  'assets/svg/notification_icon.svg',
+                  height: 24,
+                ),
               ),
             ),
           ],
@@ -60,136 +63,142 @@ class _HomePageState extends State<HomePage> {
           )),
         ),
         drawer: const DrawerWidget(),
-        body: Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    const Text(
-                      'Cards',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    Row(
-                      children: [
-                        const Text(
-                          'Add New',
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.add,
-                              size: 20,
-                            ))
-                      ],
-                    ),
-                  ],
-                ),
-                // ElevatedButton(
-                //   onPressed: () {
-                //     Navigator.of(context).push(
-                //       MaterialPageRoute(builder: (BuildContext context) {
-                //         return const LearnFlutterPage();
-                //       }),
-                //     );
-                //   },
-                //   child: const Text("Learn Flutter"),
-                // ),
-                // ElevatedButton(
-                //   onPressed: () {
-                //     Navigator.of(context).push(
-                //       MaterialPageRoute(builder: (BuildContext context) {
-                //         return const TestAPI();
-                //       }),
-                //     );
-                //   },
-                //   child: const Text("Test API"),
-                // ),
-                Stack(
-                  children: [
-                    CarouselSlider.builder(
-                      options: CarouselOptions(
-                        onPageChanged: (index, _) {
-                          _scrollIndex = index;
-                          setState(() {});
-                        },
-                        height: 210,
-                        aspectRatio: 16 / 9,
-                        viewportFraction: 0.75,
-                        initialPage: 1,
-                        autoPlay: true,
-                        enlargeCenterPage: true,
-                      ),
-                      itemCount: cardMockData.length,
-                      itemBuilder: (_, index, __) => BankCard(
-                        cardDetail: cardMockData[index],
-                      ),
-                    ),
-                    Positioned(
-                      bottom: kSpacingUnit * 2.5,
-                      left: 0,
-                      right: 0,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: cardMockData
-                            .asMap()
-                            .entries
-                            .map(
-                              (card) => AnimatedContainer(
-                                duration: const Duration(milliseconds: 300),
-                                height: 10,
-                                width: 10,
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: kSpacingUnit * 0.3),
-                                decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: _scrollIndex == card.key
-                                        ? kPrimaryColor
-                                        : Colors.transparent,
-                                    border: Border.all(
-                                      color: kPrimaryColor,
-                                      width: 1.5,
-                                    )),
-                              ),
-                            )
-                            .toList(),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Tansactions',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.more_horiz,
-                      ),
-                      color: Colors.grey,
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: transactionsMockData.length,
-                    itemBuilder: ((context, index) => TransactionItem(
-                        transaction: transactionsMockData[index])),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 10,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  const Text(
+                    'Cards',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
-                )
+                  Row(
+                    children: [
+                      const Text(
+                        'Add New',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.add,
+                            size: 18,
+                          ))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.of(context).push(
+            //       MaterialPageRoute(builder: (BuildContext context) {
+            //         return const LearnFlutterPage();
+            //       }),
+            //     );
+            //   },
+            //   child: const Text("Learn Flutter"),
+            // ),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.of(context).push(
+            //       MaterialPageRoute(builder: (BuildContext context) {
+            //         return const TestAPI();
+            //       }),
+            //     );
+            //   },
+            //   child: const Text("Test API"),
+            // ),
+            Stack(
+              children: [
+                CarouselSlider.builder(
+                  options: CarouselOptions(
+                    onPageChanged: (index, _) {
+                      _scrollIndex = index;
+                      setState(() {});
+                    },
+                    height: 210,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.75,
+                    initialPage: 1,
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                  ),
+                  itemCount: cardMockData.length,
+                  itemBuilder: (_, index, __) => BankCard(
+                    cardDetail: cardMockData[index],
+                  ),
+                ),
+                Positioned(
+                  bottom: kSpacingUnit * 2.5,
+                  left: 0,
+                  right: 0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: cardMockData
+                        .asMap()
+                        .entries
+                        .map(
+                          (card) => AnimatedContainer(
+                            duration: const Duration(milliseconds: 300),
+                            height: 10,
+                            width: 10,
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: kSpacingUnit * 0.3),
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: _scrollIndex == card.key
+                                    ? kPrimaryColor
+                                    : Colors.transparent,
+                                border: Border.all(
+                                  color: kPrimaryColor,
+                                  width: 1.5,
+                                )),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
               ],
-            )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Tansactions',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.more_horiz,
+                    ),
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: transactionsMockData.length,
+                itemBuilder: ((context, index) =>
+                    TransactionItem(transaction: transactionsMockData[index])),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/transaction_items.dart
+++ lib/widgets/transaction_items.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/data/constant.dart';
 import 'package:first_app/model/transaction_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -12,37 +13,137 @@ class TransactionItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // height: 60,
-      // color: Colors.blue,
-      margin: const EdgeInsets.all(20),
-      child: Row(
-        children: [
-          GlassmorphicContainer(
-            height: 50,
-            width: 50,
-            borderRadius: 50,
-            blur: 15,
-            alignment: Alignment.center,
-            border: 1,
-            linearGradient: LinearGradient(colors: [
-              Colors.white.withOpacity(0.2),
-              Colors.white38.withOpacity(0.2)
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-            borderGradient: LinearGradient(colors: [
-              Colors.white24.withOpacity(0.2),
-              Colors.white70.withOpacity(0.2)
-            ]),
-            child: Center(
-              child: SvgPicture.asset(
-                'assets/icon/${transaction.logo}.svg',
-                fit: BoxFit.cover,
-                height: 30,
+    return GlassmorphicContainer(
+      height: 80,
+      width: 400,
+      borderRadius: 12,
+      blur: 15,
+      alignment: Alignment.center,
+      border: 2,
+      linearGradient: LinearGradient(colors: [
+        Colors.white.withOpacity(0.2),
+        Colors.blue.withOpacity(0.1),
+      ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      borderGradient: LinearGradient(colors: [
+        Colors.white10.withOpacity(0.9),
+        Colors.white70.withOpacity(0.9),
+      ]),
+      margin: const EdgeInsets.only(left: 20, right: 20, bottom: 8),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GlassmorphicContainer(
+              height: 50,
+              width: 50,
+              borderRadius: 50,
+              blur: 15,
+              alignment: Alignment.center,
+              border: 1,
+              linearGradient: LinearGradient(colors: [
+                Colors.blue.withOpacity(0.2),
+                Colors.white38.withOpacity(0.2)
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+              borderGradient: LinearGradient(colors: [
+                Colors.white24.withOpacity(0.2),
+                Colors.white70.withOpacity(0.2)
+              ]),
+              child: Center(
+                child: SvgPicture.asset(
+                  'assets/icon/${transaction.logo}.svg',
+                  fit: BoxFit.cover,
+                  height: 30,
+                ),
               ),
             ),
-          ),
-          const Text('Test'),
-        ],
+            const SizedBox(
+              width: 10,
+            ),
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: '${transaction.title}\n',
+                    style: const TextStyle(
+                      color: Colors.blue,
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.white,
+                          spreadRadius: 2,
+                          blurRadius: kSpacingUnit * 2,
+                          offset: Offset(2, kSpacingUnit * 0.2), // cha
+                        )
+                      ],
+                    ).copyWith(
+                      fontSize: 14,
+                    ),
+                  ),
+                  TextSpan(
+                    text: transaction.description,
+                    style: const TextStyle(color: Colors.grey).copyWith(
+                      height: 1.5,
+                      fontSize: 14,
+                      shadows: [
+                        const BoxShadow(
+                          color: Colors.white,
+                          spreadRadius: 2,
+                          blurRadius: kSpacingUnit * 2,
+                          offset: Offset(2, kSpacingUnit * 0.2), // cha
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(),
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text:
+                        '${transaction.value!.isNegative ? '' : '+'}${transaction.value}\$ \n',
+                    style: const TextStyle(
+                      // color: Colors.red,
+                      shadows: [
+                        BoxShadow(
+                          color: Colors.white,
+                          spreadRadius: 2,
+                          blurRadius: kSpacingUnit * 2,
+                          offset: Offset(2, kSpacingUnit * 0.2), // cha
+                        )
+                      ],
+                    ).copyWith(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: transaction.value!.isNegative
+                          ? kRedColor
+                          : kGreenColor,
+                    ),
+                  ),
+                  TextSpan(
+                    text: transaction.date,
+                    style: const TextStyle(color: Colors.grey).copyWith(
+                      height: 1.5,
+                      fontSize: 14,
+                      shadows: [
+                        const BoxShadow(
+                          color: Colors.white,
+                          spreadRadius: 2,
+                          blurRadius: kSpacingUnit * 2,
+                          offset: Offset(2, kSpacingUnit * 0.2), // cha
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
