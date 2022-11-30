26a0f2c7cf53a0d32b0e0c52b22b4fe7316f7fba
Your Name <infor.emdiya@gmail.com>
2022-11-30 13:58:07 +0700

    Update

--- lib/widgets/transaction_items.dart
+++ lib/widgets/transaction_items.dart
@@ -1,6 +1,7 @@
 import 'package:first_app/model/transaction_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:glassmorphism/glassmorphism.dart';
 
 class TransactionItem extends StatelessWidget {
   final Transaction transaction;
@@ -11,12 +12,27 @@ class TransactionItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      width: 10,
+    return Container(
+      // height: 60,
+      // color: Colors.blue,
+      margin: const EdgeInsets.all(20),
       child: Row(
         children: [
-          Container(
+          GlassmorphicContainer(
+            height: 50,
+            width: 50,
+            borderRadius: 50,
+            blur: 15,
+            alignment: Alignment.center,
+            border: 1,
+            linearGradient: LinearGradient(colors: [
+              Colors.white.withOpacity(0.2),
+              Colors.white38.withOpacity(0.2)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            borderGradient: LinearGradient(colors: [
+              Colors.white24.withOpacity(0.2),
+              Colors.white70.withOpacity(0.2)
+            ]),
             child: Center(
               child: SvgPicture.asset(
                 'assets/icon/${transaction.logo}.svg',
@@ -24,7 +40,8 @@ class TransactionItem extends StatelessWidget {
                 height: 30,
               ),
             ),
-          )
+          ),
+          const Text('Test'),
         ],
       ),
     );
